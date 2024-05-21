cd31d89712a719540d9a3babf882047ad346cd02
Andrew Trần <Songnuoclantan@gmail.com>
2024-05-21 15:13:36 +0700

    Update FileUploadWidget.tsx

--- components/FileUploadWidget.tsx
+++ components/FileUploadWidget.tsx
@@ -1,58 +1,109 @@
+import { useCustomToast } from '@/hooks';
 import { Box, Text } from '@chakra-ui/react';
-import React, { useRef, useState } from 'react';
+import styled from '@emotion/styled';
+import { useRef, useState } from 'react';
 
-const FileUploadWidget = ({ id, required, readonly, disabled, value, onChange, label }: any) => {
+const FileUploadWidget = ({ id, required, readonly, disabled, value, onChange, label, ...props }: any) => {
   const [preview, setPreview] = useState(value);
 
   const refInput = useRef<any>(null);
 
+  const { toastFail } = useCustomToast();
+
+  const fileTypeAccepted = props.schema.fileType;
+
   const handleFileChange = (event: any) => {
     const file = event.target.files[0];
     if (file) {
-      const reader = new FileReader();
+      if (fileTypeAccepted && !fileTypeAccepted.includes(file?.type)) {
+        toastFail({
+          title: `Image files must be in the following formats: ${fileTypeAccepted}`
+        });
+        return;
+      }
+      const reader: any = new FileReader();
       reader.onloadend = () => {
         const base64String = reader.result.split(',')[1];
+        const fileType = `${file.type}`;
         setPreview(reader.result);
-        onChange(base64String); // Change here to store base64 string
+        onChange(base64String + `$_$${fileType}`); // Change here to store base64 string
       };
       reader.readAsDataURL(file);
     }
   };
 
+  const handleDrop = (e: React.DragEvent<HTMLDivElement>) => {
+    e.preventDefault();
+    const file = e.dataTransfer.files[0];
+    if (file) {
+      if (!file.type || (fileTypeAccepted && !fileTypeAccepted.includes(file.type))) {
+        toastFail({
+          title: `Image files must be in the following formats: ${fileTypeAccepted}`
+        });
+        return;
+      }
+      const reader: any = new FileReader();
+      reader.onloadend = () => {
+        const base64String = reader.result.split(',')[1];
+        const fileType = `${file.type}`;
+        setPreview(reader.result);
+        onChange(base64String + `$_$${fileType}`); // Change here to store base64 string
+      };
+      reader.readAsDataURL(file);
+    }
+  };
+
+  const getFileForm = () =>{
+      case 'file':
+        if (previousUrlFile === dataChanges[el]) {
+          break;
+        }
+        if (!dataChanges[el]) {
+          obj.file = null;
+          break;
+        }
+        const arrData = dataChanges[el].split('$_$');
+        const base64String = arrData[0];
+        const fileType = arrData[1] ?? 'image/png';
+
+        // Chuyển đổi base64 string trở lại thành file
+        const byteCharacters = atob(base64String);
+        const byteNumbers = new Array(byteCharacters.length);
+        for (let i = 0; i < byteCharacters.length; i++) {
+          byteNumbers[i] = byteCharacters.charCodeAt(i);
+        }
+        const byteArray = new Uint8Array(byteNumbers);
+        const blob = new Blob([byteArray], { type: fileType }); // Cập nhật type tùy thuộc vào loại ảnh của bạn
+        const file = new File([blob], 'logo.png', { type: fileType });
+        obj.file = file;
+        break;
+  }
+
+  const overlayDrop = useRef<HTMLDivElement | null>(null);
+
   const onOpen = () => {
     refInput.current?.click();
   };
-
-  /*
-    EXAMPLE FOR UPLOAD
-    case 'file':
-      if (data?.media?.fullUrlSmall === dataChanges[el]) {
-        break;
-      }
-      if (!dataChanges[el]) {
-        obj.file = null;
-        break;
-      }
-      const base64String = dataChanges[el];
-
-      // Chuyển đổi base64 string trở lại thành file
-      const byteCharacters = atob(base64String);
-      const byteNumbers = new Array(byteCharacters.length);
-      for (let i = 0; i < byteCharacters.length; i++) {
-        byteNumbers[i] = byteCharacters.charCodeAt(i);
-      }
-      const byteArray = new Uint8Array(byteNumbers);
-      const blob = new Blob([byteArray], { type: 'image/png' }); // Cập nhật type tùy thuộc vào loại ảnh của bạn
-      const file = new File([blob], 'logo.png', { type: 'image/png' });
-      obj.file = file;
-      break;
-  */
   return (
     <div
       style={{
         display: 'flex',
         flexDirection: 'column'
       }}
+      onDrop={(e) => {
+        e.preventDefault();
+        handleDrop(e);
+        if (overlayDrop.current) {
+          overlayDrop.current.style.display = 'none';
+        }
+      }}
+      onDragLeave={(e) => {
+        e.preventDefault();
+        if (overlayDrop.current) {
+          overlayDrop.current.style.display = 'none';
+        }
+      }}
+      onDragOver={(e) => e.preventDefault()}
     >
       <label className='css-g6pte' htmlFor={id}>
         {label}
@@ -65,8 +116,16 @@ const FileUploadWidget = ({ id, required, readonly, disabled, value, onChange, l
         required={required}
         disabled={disabled || readonly}
         onChange={handleFileChange}
+        accept={fileTypeAccepted}
       />
-      <Box position={'relative'} onClick={onOpen} cursor={'pointer'}>
+      <Box
+        position={'relative'}
+        onClick={onOpen}
+        cursor={'pointer'}
+        style={{
+          width: 'fit-content'
+        }}
+      >
         {preview ? (
           <img src={preview} alt='Preview' style={{ width: '100px', borderRadius: 4 }} />
         ) : (
@@ -88,5 +147,16 @@ const FileUploadWidget = ({ id, required, readonly, disabled, value, onChange, l
     </div>
   );
 };
+const DropImageHere = styled.div`
+  @keyframes scale {
+    from {
+      transform: scale(0.9);
+    }
+    to {
+      transform: scale(1.3);
+    }
+  }
+  animation: scale 1.5s cubic-bezier(0.68, -0.55, 0.265, 1.55) infinite alternate;
+`;
 
 export default FileUploadWidget;
